114368220baa2deb6bb7c0e3ea840c412a7e62ac
FallenShard <nemanjabartolovic@yahoo.com>
2015-02-07 01:44:51 +0100

    Start fixing the main page

--- GamingPlatform/App_Code/Service.cs
+++ GamingPlatform/App_Code/Service.cs
@@ -8,63 +8,41 @@ using System.Text;
 
 using Neo4jClient;
 using Neo4jClient.Cypher;
+using System.ServiceModel.Activation;
+using System.Security.Cryptography;
 
 // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
 public class Service : IService
 {
-    public class Actor
+    public string GetUserSessionToken(string username, string password)
     {
-        public String id { get; set; }
-        public String name { get; set; }
-        public String birthplace { get; set; }
-        public String birthday { get; set; }
-        public String biography { get; set; }
+        string token = "guest";
 
-        public DateTime getBirthday()
-        {
-            if (this.birthday == null) return new DateTime();
-
-            long timestamp = Int64.Parse(this.birthday);
-            DateTime startDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0);
-            return startDateTime.AddMilliseconds(timestamp).ToLocalTime();
-        }
-    }
-
-	public string GetData(int value)
-	{
         GraphClient client = new GraphClient(new Uri("http://localhost:7474/db/data"));
         client.Connect();
 
-        string actorName = ".*" + "Tom" + ".*";
+        var results = client.Cypher.
+            Match("(user:User)").
+            Where((User user) => user.username == username).
+            AndWhere((User user) => user.password == password).
+            Return(user => user.As<User>()).Results;
 
-        Dictionary<string, object> queryDict = new Dictionary<string, object>();
-        queryDict.Add("actorName", actorName);
-
-        var query = new Neo4jClient.Cypher.CypherQuery("start n=node(*) match (n:Person) where has(n.name) and n.name =~ {actorName} return n",
-                                                        queryDict, CypherResultMode.Set);
-
-        List<Actor> actors = ((IRawGraphClient)client).ExecuteGetCypherResults<Actor>(query).ToList();
-
-        foreach (Actor a in actors)
+        if (results.Count() > 0)
         {
-            //DateTime bday = a.getBirthday();
-            //MessageBox.Show(a.name);
-            string s = a.name;
+            User result = results.First();
+            token = result.username + result.password;
         }
 
-		return string.Format("You entered: {0}", value);
-	}
+        return CreateSHAHash(token);
+    }
 
-	public CompositeType GetDataUsingDataContract(CompositeType composite)
-	{
-		if (composite == null)
-		{
-			throw new ArgumentNullException("composite");
-		}
-		if (composite.BoolValue)
-		{
-			composite.StringValue += "Suffix";
-		}
-		return composite;
-	}
+    public static string CreateSHAHash(string Phrase)
+    {
+        SHA256Managed HashTool = new SHA256Managed();
+        Byte[] PhraseAsByte = System.Text.Encoding.UTF8.GetBytes(string.Concat(Phrase));
+        Byte[] EncryptedBytes = HashTool.ComputeHash(PhraseAsByte);
+        HashTool.Clear();
+        return Convert.ToBase64String(EncryptedBytes);
+    }
 }
--- GamingPlatform/scripts/test.js
+++ GamingPlatform/scripts/test.js
@@ -1,14 +1,28 @@
-﻿"use strict";
+﻿
 (function () {
-
-    var userLoggedIn = "Guest";
+    'use strict';
+    //var userLoggedIn = "Guest";
 
     var documentInit = function () {
 
-       // $("#rateYo").rateYo({
+        // $("#rateYo").rateYo({
         //    rating: 3.6
         //});
 
+        $.ajax({
+            type: "GET",
+            url: "Service.svc/GetUserSessionToken",
+            data: { username: "admin", password: "admin" },
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            processData: true,
+            success: function (receivedData) {
+                console.log("Received: " + receivedData);
+            },
+            error: function (result) {
+                console.log("Error performing ajax " + result);
+            }
+        });
 
         var x = getQueryVariable("id");
         if (x)
@@ -16,14 +30,12 @@
 
         console.log(sessionStorage.userName);
 
-        if (sessionStorage.userName)
-        {
+        if (sessionStorage.userName) {
             $("#log-in-title").html("Logged in as " + sessionStorage.userName + " from session");
 
             $("#log-in-style").attr("style", "color: #FF0000");
         }
-        else
-        {
+        else {
             sessionStorage.userName = "Bob";
             $("#log-in-title").html("Logged in as Bob fresh");
 
@@ -32,18 +44,19 @@
 
         $("#redirect-button").click(function () {
             window.location.href = "index.html?id=333";
-            
+
         });
-    }
+    };
 
     function getQueryVariable(variable) {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
+        var query = window.location.search.substring(1),
+            vars = query.split("&");
         for (var i = 0; i < vars.length; i++) {
             var pair = vars[i].split("=");
-            if (pair[0] == variable) { return pair[1]; }
+            if (pair[0] === variable) { return pair[1]; }
         }
         return (false);
+        
     }
 
     $(document).ready(documentInit);
